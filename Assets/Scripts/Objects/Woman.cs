a1007f28155e730d487a82b47a5349153a092840
苗木谜梦 <384970327@qq.com>
2022-01-22 23:41:09 +0800

    1

--- Assets/Scripts/Objects/Woman.cs
+++ Assets/Scripts/Objects/Woman.cs
@@ -7,21 +7,22 @@ public class Woman : MonoBehaviour
     public static bool isFrameFull, hasDiary;
     public Television television;
     public Door door;
-    
+    public Animator anim;
+
     public void EnterRoom()
     {
         //进入房间的动画
         door.isOpen = true;
-        
+
         //判断是否看到完整的结婚照
         if (isFrameFull)
         {
             //望向相框的动画
-            
+
             if (hasDiary)
             {
                 //阅读日记并看向男方的动画
-                
+
             }
         }
         else
@@ -37,13 +38,27 @@ public class Woman : MonoBehaviour
         yield return new WaitForSeconds(30f);
         ExitRoom();
     }
-    
+
     void ExitRoom()
     {
         //离开房间的动画
-        
+
         //电视机状态切换
         television.SwitchStatus();
     }
-    
+    private GameObject rb;
+    private void Awake() {
+    rb=GetComponent<GameObject>();
+    }
+    private void FixedUpdate() {
+        Move();
+    }
+    void Move(){
+        float horizontalmove=Input.GetAxisRaw("Horizontal");
+        bool on=Input.GetKey(KeyCode.W);
+        bool move=Input.GetKey(KeyCode.S);
+        anim.SetFloat("right",Mathf.Abs(horizontalmove));
+        anim.SetBool("on",on);
+        anim.SetBool("move",move);
+    }
 }
