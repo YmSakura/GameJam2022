eddfaa475d6fe6f4364b913f72499a96e0d04a23
NoneAndNever <43804135+NoneAndNever@users.noreply.github.com>
2022-01-23 02:31:21 +0800

    物品合成和物品提示完成

--- Assets/Inventory/ItemDrag.cs
+++ Assets/Inventory/ItemDrag.cs
@@ -1,13 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ItemDrag : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragHandler
+public class ItemDrag : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    public Item FixedPhoto;
+    public InventoryManager inventoryManager;
+    public GameObject InfoPanel;                    //物品说明提示界面
     public Transform originalParent;
     public Inventory inventory;
     private int currentItemID;//当前物品ID
+
+    private void Awake()
+    {
+        originalParent=transform.parent;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        var item = inventory.itemList[gameObject.transform.parent.GetComponent<Slot>().slotID];
+        InfoPanel.SetActive(true);
+        InfoPanel.transform.GetChild(0).GetComponentInChildren<Text>().text = 
+            "物品名称:" + item.itemName + "\n" + "物品介绍:" + item.itemInfo;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        InfoPanel.SetActive(false);
+    }
+
+
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -29,44 +54,56 @@ public class ItemDrag : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragH
         GameObject pointGameObject = eventData.pointerCurrentRaycast.gameObject;
 
         if (!pointGameObject||pointGameObject.gameObject==originalParent.gameObject)
-        {
-            transform.SetParent(originalParent);
-            transform.position = originalParent.position;
-        }
+            resetPosition();
         else if (pointGameObject.name == "Item Image")//判断下面物体名字是：Item Image 那么互换位置
         {
             Transform pointFather = pointGameObject.transform.parent.parent;
-            
-            transform.SetParent(pointFather);
-            transform.position = pointFather.position;
+            int targetID = pointFather.GetComponent<Slot>().slotID;
+            if (inventory.itemList[targetID].itemName == "结婚照" && inventory.itemList[currentItemID].itemName == "相框"
+                || inventory.itemList[targetID].itemName == "相框" && inventory.itemList[currentItemID].itemName == "结婚照")
+            {
+                resetPosition();
+                inventory.itemList[currentItemID] = null;
+                originalParent.GetChild(0).gameObject.SetActive(false);
+                inventory.itemList[targetID] = FixedPhoto;
+                inventoryManager.RefreshSlot();
+                GetComponent<CanvasGroup>().blocksRaycasts = true;
+                return;
+            }
+            resetPosition();
+            Sprite originalImage = GetComponentInChildren<Image>().sprite;
+            GetComponentInChildren<Image>().sprite = pointGameObject.GetComponent<Image>().sprite;
+            pointGameObject.GetComponent<Image>().sprite = originalImage;
             //itemList的物品存储位置改变
-            var temp = inventory.itemList[currentItemID];
-            inventory.itemList[currentItemID] = inventory.itemList[pointGameObject.GetComponentInParent<Slot>().slotID];
-            inventory.itemList[pointGameObject.GetComponentInParent<Slot>().slotID] = temp;
-
-            pointGameObject.transform.parent.position = originalParent.position;
-            pointGameObject.transform.parent.SetParent(originalParent);
+            targetID = pointGameObject.GetComponentInParent<Slot>().slotID;
+            //交换背包列表中的内容
+            (inventory.itemList[currentItemID], inventory.itemList[targetID]) = (inventory.itemList[targetID], inventory.itemList[currentItemID]);
+            
             GetComponent<CanvasGroup>().blocksRaycasts = true;//射线阻挡开启，不然无法再次选中移动的物品
             return;
         }
         else if (pointGameObject.name.Substring(0, 4) == "Slot") //判断下面物体名字是：Slot 则直接挂在检测到到Slot下面
         {
-            var originItem = pointGameObject.transform.GetChild(0).gameObject;
-            originItem.transform.SetParent(originalParent);
-            transform.SetParent(pointGameObject.transform);
-            transform.position = pointGameObject.transform.position;
+            resetPosition();
+            
+            Sprite image = GetComponentInChildren<Image>().sprite;
+            originalParent.GetChild(0).gameObject.SetActive(false);
+            pointGameObject.transform.GetChild(0).gameObject.SetActive(true);
+            pointGameObject.transform.GetChild(0).GetComponentInChildren<Image>().sprite = image;
             //itemList的物品存储位置改变
             inventory.itemList[pointGameObject.GetComponentInParent<Slot>().slotID] = inventory.itemList[currentItemID];
             inventory.itemList[currentItemID] = null;
         }
         else
-        {
-            transform.SetParent(originalParent);
-            transform.position = originalParent.position;
-        }
+            resetPosition();
         
 
         GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 
+    private void resetPosition()//如果移动失败,物品归位移动到本来的框
+    {
+        transform.SetParent(originalParent);
+        transform.position = originalParent.position;
+    }
 }
