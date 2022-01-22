2ef641f192197d84f9dbc9d6fb3928bc7efb4ffa
苗木谜梦 <384970327@qq.com>
2022-01-22 16:24:20 +0800

    dialog

--- Assets/code/Dialog.cs
+++ Assets/code/Dialog.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Dialog : MonoBehaviour
 {
     public GameObject dialog1;
-    private void Update() {
-        if(Input.GetKeyDown("space")){
-        dialog1.SetActive(false);
+    public GameObject dialog2;
+    public GameObject dialog3;
+    private bool dialog2T = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            dialog1.SetActive(false);
+        }
+        if (Input.GetKeyDown("space") && dialog2T == true)
+        {
+            dialog2.SetActive(false);
+        }
     }
-    }
-    
 }
