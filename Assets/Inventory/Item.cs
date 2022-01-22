12ea4a8a287ad1f78bfa866640418d36acb6a88c
NoneAndNever <43804135+NoneAndNever@users.noreply.github.com>
2022-01-22 14:41:35 +0800

    backpackUI
    背包UI完成

---
+++ Assets/Inventory/Item.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item",menuName = "Inventory/New Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;                     //物品名称
+    public Sprite itemImage;                    //物品图片
+    [TextArea] public string itemInfo;          //物品说明
+    public bool isInteractable;                 //物品是否可以交互
+}
